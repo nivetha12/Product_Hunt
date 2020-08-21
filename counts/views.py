0ad3d8f8000969e47239e066e04026b72b079b94
nivetha k <nivinataliya@gmail.com>
2020-08-21 21:32:27 +0530

    signup created

--- counts/views.py
+++ counts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def signup(request):
+    if request.method=='POST':
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request,'signup.html',{'error':'username has already taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+    else:
+        return render(request,'signup.html',{'error':'PASSWORD MUST MATCH'})
+
+
+def login(request):
+    return render(request,'login.html')
+
+def logout(request):
+    return render(request,'signup.html')
+
+
